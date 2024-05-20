1e5991a73583906b1647876f3ac3e1bcf2d6ba23
Junanjunan <taltalrealty@gmail.com>
2024-05-20 16:23:42 +0900

    [add] direction home url logo

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, HStack } from "@chakra-ui/react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Root() {
   return (
@@ -10,6 +10,12 @@ export default function Root() {
         px={10}
         borderBottomWidth={1}
       >
+        <Link to={"/"}>
+          <img
+            src={`${process.env.PUBLIC_URL}/g6_logo.png`} alt="logo"
+            style={{ width: "50px", height: "50px" }}
+          />
+        </Link>
         <HStack spacing={2}>
           <Button>Log in</Button>
           <Button colorScheme={"blue"}>Sign up</Button>
