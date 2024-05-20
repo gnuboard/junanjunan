2f6d5538489a10773b055f03ad377761246cafca
Junanjunan <taltalrealty@gmail.com>
2024-05-20 15:33:32 +0900

    [add] router provider instead of App with react-router-dom

---
+++ src/router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./components/Root";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  }
+]);
+
+
+export default router;
