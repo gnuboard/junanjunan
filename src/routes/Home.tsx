895076623c50bbd1acefa2873f2813e16f33e944
Junanjunan <taltalrealty@gmail.com>
2024-05-21 11:26:05 +0900

    [add] several writes component in writes list with Array map

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -18,34 +18,36 @@ export default function Home() {
       rowGap={8}
       templateColumns={"repeat(5, 1fr)"}
     >
-      <VStack alignItems={"flex-start"}>
-        <Box position="relative" overflow={"hidden"} mb={3} rounded="3xl">
-          <Image
-            h="280"
-            src="https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720"
-          />
-          <Button variant={"unstyled"} position="absolute" top={0} right={0} color="white">
-            <FaRegHeart size={20} />
-          </Button>
-        </Box>
-        <Box>
-          <Grid gap={2} templateColumns={"6fr 1fr"}>
-            <Text display={"block"} as="b" noOfLines={1} fontSize="md">
-              Cheomdangwahak-ro,Jeongeup-si, North Jeolla Province, South Korea
+      {[...Array(20)].map(index => (
+        <VStack alignItems={"flex-start"}>
+          <Box position="relative" overflow={"hidden"} mb={3} rounded="3xl">
+            <Image
+              h="280"
+              src="https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720"
+            />
+            <Button variant={"unstyled"} position="absolute" top={0} right={0} color="white">
+              <FaRegHeart size={20} />
+            </Button>
+          </Box>
+          <Box>
+            <Grid gap={2} templateColumns={"6fr 1fr"}>
+              <Text display={"block"} as="b" noOfLines={1} fontSize="md">
+                Cheomdangwahak-ro,Jeongeup-si, North Jeolla Province, South Korea
+              </Text>
+              <HStack spacing={1}>
+                <FaStar size={15} />
+                <Text>5.0</Text>
+              </HStack>
+            </Grid>
+            <Text fontSize={"sm"} color="gray.600">
+              Seoul, S. Korea
             </Text>
-            <HStack spacing={1}>
-              <FaStar size={15} />
-              <Text>5.0</Text>
-            </HStack>
-          </Grid>
+          </Box>
           <Text fontSize={"sm"} color="gray.600">
-            Seoul, S. Korea
+            <Text as="b">$72</Text> / night
           </Text>
-        </Box>
-        <Text fontSize={"sm"} color="gray.600">
-          <Text as="b">$72</Text> / night
-        </Text>
-      </VStack>
+        </VStack>
+      ))}
     </Grid>
   );
 }
