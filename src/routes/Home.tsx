2ee4ecbb95c1d264f644d5ca483a2f23fcb19ee8
Junanjunan <taltalrealty@gmail.com>
2024-05-21 11:46:52 +0900

    [fix] Overflow of text under image

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         <VStack alignItems={"flex-start"}>
           <Box position="relative" overflow={"hidden"} mb={3} rounded="3xl">
             <Image
-              h="280"
+              minH="280"
               src="https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720"
             />
             <Button variant={"unstyled"} position="absolute" top={0} right={0} color="white">
