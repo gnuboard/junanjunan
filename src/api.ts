01ea52b708416dd0e5443f3568f74124cf4802be
Junanjunan <taltalrealty@gmail.com>
2024-05-22 15:04:28 +0900

    [refactor] fetching data with axios instnace

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/api/v1";
+
+const axiosInstance = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1"
+})
+
 
 export async function getWrites() {
-  const response = await axios.get(`${BASE_URL}/boards/free/writes`);
+  const response = await axiosInstance.get("/boards/free/writes");
   return response.data;
 }
