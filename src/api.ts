1b151b315891d98f0700dc5c27127cf0b035da0a
Junanjunan <taltalrealty@gmail.com>
2024-05-22 14:39:40 +0900

    [refactor] fetching data function with axios

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
+import axios from "axios";
+
 const BASE_URL = "http://127.0.0.1:8000/api/v1";
 
 export async function getWrites() {
-  const response = await fetch(`${BASE_URL}/boards/free/writes`);
-  const json = await response.json();
-  return json;
+  const response = await axios.get(`${BASE_URL}/boards/free/writes`);
+  return response.data;
 }
