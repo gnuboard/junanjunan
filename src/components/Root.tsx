070f30c28ba7f1e33379f0df8b1fde92bb4b139f
Junanjunan <taltalrealty@gmail.com>
2024-05-22 16:20:20 +0900

    [add] react-query-devtools in Root component

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,6 +1,7 @@
 
 import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Header from "./Header";
 
 
@@ -9,6 +10,7 @@ export default function Root() {
     <Box>
       <Header />
       <Outlet />
+      <ReactQueryDevtools />
     </Box>
   );
 }
