f528cc0b09b087a08c1cf11c216d283335e4c96b
Junanjunan <taltalrealty@gmail.com>
2024-05-23 09:54:17 +0900

    [add] WriteDetail image with Grid(+GridItem)

--- src/routes/WriteDetail.tsx
+++ src/routes/WriteDetail.tsx
@@ -2,7 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getWrite } from "../api";
 import { IWrite } from "../types";
+import { Box, Grid, Image, GridItem, Skeleton, Heading } from "@chakra-ui/react";
 
+
+function get_img_url (bf_file: string) {
+  return `http://localhost:8000/${bf_file}`;
+}
 
 export default function WriteDetail() {
   const { wr_id } = useParams();
@@ -10,5 +15,35 @@ export default function WriteDetail() {
     queryKey: ["write", wr_id],
     queryFn: getWrite
   });
-  return <div>WriteDetail</div>;
+  return (
+    <Box
+      mt={10}
+      px={{ base: 10, lg: 40 }}
+    >
+      <Heading>{ data?.wr_subject }</Heading>
+      <Grid
+        mt={8}
+        rounded="xl"
+        overflow={"hidden"}
+        gap={2}
+        height="100vh"
+        templateRows={"1fr 1fr"}
+        templateColumns={"repeat(4, 1fr)"}
+      >
+        {data?.images.map((img, index) => (
+          <GridItem
+            colSpan={index === 0 ? 2 : 1}
+            rowSpan={index === 0 ? 2 : 1}
+            overflow={"hidden"}
+            key={img.bf_source}
+            gridColumn={"span 4"}
+          >
+            <Skeleton isLoaded={!isLoading} h="100%" w="100%">
+              <Image w="100%" h="100%" objectFit={"cover"} src={get_img_url(img.bf_file)}/>
+            </Skeleton>
+          </GridItem>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
