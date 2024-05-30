2b7bc10300c46f783dc207f39701359ce87ae527
Junanjunan <taltalrealty@gmail.com>
2024-05-30 17:04:24 +0900

    [add] Allow access update write page only writer

--- src/routes/WriteUpdate.tsx
+++ src/routes/WriteUpdate.tsx
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
 import { Text } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import { IWrite } from "../types";
@@ -6,15 +7,21 @@ import { getWrite } from "../api";
 
 
 export default function WriteUpdate() {
+  const loginUserMbID = useSelector((state: any) => state.loginUser).mb_id;
+  const navigate = useNavigate();
   const { wr_id } = useParams();
   const { isLoading, data } = useQuery<IWrite>({
     queryKey: ["write", wr_id],
     queryFn: getWrite
   });
-  console.log(data);
+  const wrMbId = data ? data.mb_id : "";
 
   if (isLoading) {
     return <Text>Loading...</Text>;
+  }
+
+  if (loginUserMbID !== wrMbId) {
+    navigate("/");
   }
 
   return <Text>Update Write</Text>;
