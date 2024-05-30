5b04528cd49496e19455a90065b0ac84afc95ea4
Junanjunan <taltalrealty@gmail.com>
2024-05-30 16:59:37 +0900

    [add] Fetch data in write update page

--- src/routes/WriteUpdate.tsx
+++ src/routes/WriteUpdate.tsx
@@ -1,6 +1,16 @@
+import { useParams } from "react-router-dom";
 import { Text } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { IWrite } from "../types";
+import { getWrite } from "../api";
 
 
 export default function WriteUpdate() {
+  const { wr_id } = useParams();
+  const { data } = useQuery<IWrite>({
+    queryKey: ["write", wr_id],
+    queryFn: getWrite
+  });
+  console.log(data);
   return <Text>Update Write</Text>;
 }
