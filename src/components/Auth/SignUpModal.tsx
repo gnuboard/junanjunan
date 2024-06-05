dbfc9d66306a3c6f918c689e26d71341f8bef9b4
Junanjunan <taltalrealty@gmail.com>
2024-06-05 18:06:35 +0900

    [add] Second slide UI of Signup modal with required field

--- src/components/Auth/SignUpModal.tsx
+++ src/components/Auth/SignUpModal.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import {
   Box, Button, Input, InputGroup, InputLeftElement, Modal, VStack,
   ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay,
+  Heading,
 } from "@chakra-ui/react";
 import { FaUserNinja, FaLock, FaEnvelope, FaUserSecret } from "react-icons/fa";
 import { motion, AnimatePresence } from 'framer-motion';
@@ -28,6 +29,7 @@ export default function SignUpModal({ isOpen, onClose }: ISignUpModalProps) {
       id: 2,
       content: (
         <VStack>
+          <Heading size={"sm"}>사이트 이용정보 입력</Heading>
             <InputGroup>
               <InputLeftElement
                 children={
@@ -36,27 +38,7 @@ export default function SignUpModal({ isOpen, onClose }: ISignUpModalProps) {
                   </Box>
                 }
               />
-              <Input variant={"filled"} placeholder="Name" />
-            </InputGroup>
-            <InputGroup>
-              <InputLeftElement
-                children={
-                  <Box color="gray.500">
-                    <FaEnvelope />
-                  </Box>
-                }
-              />
-              <Input variant={"filled"} placeholder="Email" />
-            </InputGroup>
-            <InputGroup>
-              <InputLeftElement
-                children={
-                  <Box color="gray.500">
-                    <FaUserNinja />
-                  </Box>
-                }
-              />
-              <Input variant={"filled"} placeholder="Username" />
+              <Input variant={"filled"} placeholder="아이디(필수)" />
             </InputGroup>
             <InputGroup>
               <InputLeftElement
@@ -66,7 +48,48 @@ export default function SignUpModal({ isOpen, onClose }: ISignUpModalProps) {
                   </Box>
                 }
               />
-              <Input variant={"filled"} placeholder="Password" />
+              <Input variant={"filled"} placeholder="비밀번호(필수)" />
+            </InputGroup>
+            <InputGroup>
+              <InputLeftElement
+                children={
+                  <Box color="gray.500">
+                    <FaLock />
+                  </Box>
+                }
+              />
+              <Input variant={"filled"} placeholder="비밀번호 확인(필수)" />
+            </InputGroup>
+            <Heading size={"sm"} marginTop={"10px"}>개인정보 입력</Heading>
+            <InputGroup>
+              <InputLeftElement
+                children={
+                  <Box color="gray.500">
+                    <FaUserNinja />
+                  </Box>
+                }
+              />
+              <Input variant={"filled"} placeholder="이름 (필수)" />
+            </InputGroup>
+            <InputGroup>
+              <InputLeftElement
+                children={
+                  <Box color="gray.500">
+                    <FaUserNinja />
+                  </Box>
+                }
+              />
+              <Input variant={"filled"} placeholder="닉네임 (필수)" />
+            </InputGroup>
+            <InputGroup>
+              <InputLeftElement
+                children={
+                  <Box color="gray.500">
+                    <FaEnvelope />
+                  </Box>
+                }
+              />
+              <Input variant={"filled"} placeholder="E-mail (필수)" />
             </InputGroup>
           </VStack>
       )
