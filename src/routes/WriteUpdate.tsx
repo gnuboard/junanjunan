bf1e7efd1b765586fe13e5bf486cc6076859a3a0
Junanjunan <taltalrealty@gmail.com>
2024-06-05 10:17:58 +0900

    [refactor] Use defaultValues of useForm instead of useState

--- src/routes/WriteUpdate.tsx
+++ src/routes/WriteUpdate.tsx
@@ -10,27 +10,32 @@ import { useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
 import { IRequestWriteUpdate, IRootState } from "../types";
 import { updateWrite } from "../api";
-import { useState } from "react";
 
 
 export default function WriteUpdate() {
   const loginUserMbID = useSelector((state: any) => state.loginUser).mb_id;
   const access_token = useSelector((state: IRootState) => state.token).access_token;
   const navigate = useNavigate();
-  const { register, handleSubmit } = useForm<IRequestWriteUpdate>();
+  const { wr_id } = useParams();
+  const { data } = useQueryGetWrite(Number(wr_id));
+  const { register, handleSubmit } = useForm<IRequestWriteUpdate>({
+    defaultValues: {
+      access_token: access_token ? access_token : "",
+      wr_id: wr_id,
+      variables: {
+        wr_subject: data?.wr_subject,
+        wr_content: data?.wr_content,
+        wr_link1: data?.wr_link1,
+        wr_link2: data?.wr_link2
+      }
+    }
+  });
   const mutation = useMutation({
     mutationFn: updateWrite,
     onSuccess: () => {},
     onError: () => {}
   });
-  const { wr_id } = useParams();
-  const { data } = useQueryGetWrite(Number(wr_id));
   const wrMbId = data ? data.mb_id : "";
-  const [wrSubject, setWrSubject] = useState(data?.wr_subject);
-  const [wrContent, setWrContent] = useState(data?.wr_content);
-  const [wrLink1, setWrLink1] = useState(data?.wr_link1);
-  const [wrLink2, setWrLink2] = useState(data?.wr_link2);
-  
 
   if (loginUserMbID !== wrMbId) {
     navigate("/");
@@ -44,24 +49,20 @@ export default function WriteUpdate() {
   return (
     <Container mt={10} px={{ base: 10, lg: 40 }}>
       <VStack as="form" onSubmit={handleSubmit(onSubmit)}>
-        <Input {...register("access_token")} required type="text" value={access_token ? access_token : ""} hidden/>
-        <Input {...register("wr_id")} required type="text" value={wr_id} hidden/>
+        <Input {...register("access_token")} required type="text" hidden/>
+        <Input {...register("wr_id")} required type="text" hidden/>
         <FormControl>
           <FormLabel>제목</FormLabel>
           <Input
            {...register("variables.wr_subject", { required: true })}
            required
            type="text"
-           value={wrSubject}
-           onChange={(e) => setWrSubject(e.target.value)}
           />
         </FormControl>
         <FormControl>
           <Textarea
            {...register("variables.wr_content", { required: true })}
            required
-           value={wrContent}
-           onChange={(e) => setWrContent(e.target.value)}
           />
         </FormControl>
         <FormControl>
@@ -70,8 +71,6 @@ export default function WriteUpdate() {
             <Input
               {...register("variables.wr_link1")}
               type="text"
-              value={wrLink1}
-              onChange={(e) => setWrLink1(e.target.value)}
             />
           </InputGroup>
         </FormControl>
@@ -81,8 +80,6 @@ export default function WriteUpdate() {
             <Input
               {...register("variables.wr_link2")}
               type="text"
-              value={wrLink2}
-              onChange={(e) => setWrLink2(e.target.value)}
             />
           </InputGroup>
         </FormControl>
