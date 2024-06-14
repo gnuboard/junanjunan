fbec90beefd327e1cce2281f44c16cb4cf3bf662
Junanjunan <taltalrealty@gmail.com>
2024-06-14 09:55:39 +0900

    [chore] Add linebreak in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,7 @@ const client = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <ReduxProvider store={store}>
