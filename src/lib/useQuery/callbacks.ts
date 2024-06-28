5dddf262a43eb5ea5cd90f902736e8b57a1d6fcd
Junanjunan <taltalrealty@gmail.com>
2024-06-28 10:28:24 +0900

    [fix] Error handling condition when getWrite
    Depending on changed response from api server, error handling condition is fixed

--- src/lib/useQuery/callbacks.ts
+++ src/lib/useQuery/callbacks.ts
@@ -3,7 +3,7 @@ import { AxiosError } from "axios";
 
 export function getWriteRetryCallback(failureCount: number, error: any) {
   if (error instanceof AxiosError) {
-    if (error.response?.data.detail !== "Token has expired") {
+    if (!error.response?.data.detail.includes("Token has expired")) {
       alert(error.response?.data.detail);
       window.history.back();
     }
